c3c109db83ed8981d35907db937c5553a4914b67
Urieel <urielsalazar.dev@gmail.com>
2026-02-02 12:02:25 -0600

    File added  for validating input from the user

--- Menus/basic_menu.py
+++ Menus/basic_menu.py
@@ -1,19 +1,23 @@
 from api_config import open_link,secret_env,file_exist
 from Flask.flask_config import app
 from attempt import see_user
+from Validate.validate_input import verify_number
 
 
 def menu():
+    """ Main menu where user can log in and get access to the action's menu.
+    """
     while True:
+        print("===== Popcorn Stats =====")
         print(" 1. Log in ")
-        print("2. Menu Trakt")
+        print("2. Menu ")
         
-        option=int(input("Select an option : "))
-             
+        option=verify_number("Select an option : ")
         if option == 1:
             client_id,__=secret_env()
             open_link(client_id)
             app.run(port=8000)
+            print("Log in completed ! 🎉")
             
         if option==2:
             menu_options()
@@ -23,10 +27,10 @@ def menu_options():
     file=file_exist()
     if file:
         while True:
-            print("1. See your user")
-            print("2.")
-            print("3.")
-            choose_action=int(input("Please, select an option :"))
+            print("1. Endpoint")
+            print("2. Endpoint")
+            print("3. Endpoint")
+            choose_action=verify_number("Please, select an option :")
         
             if choose_action==1:
                 see_user()
